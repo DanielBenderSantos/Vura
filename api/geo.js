6c2eb31660177736ee7322087253fa140b054075
Daniel Bender <danielbendersantos@gmail.com>
2026-02-13 13:52:23 -0300

    ajuste cors

--- api/geo.js
+++ api/geo.js
@@ -5,7 +5,11 @@
 // - longitude
 // - timezone (IANA, ex: "America/Sao_Paulo")
 // Assim o front consegue gerar o mapa astral com dados corretos.
+res.setHeader("Access-Control-Allow-Origin", "https://danielbendersantos.github.io");
+res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+if (req.method === "OPTIONS") return res.status(200).end();
 import tzlookup from "tz-lookup";
 
 export default async function handler(req, res) {
